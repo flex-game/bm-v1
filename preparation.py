1f3c4cffe7f4e14feba28256c8e8da29fce60502
Jonathan Holtby <holtby.jonathan@gmail.com>
2024-11-12 16:32:02 +0000

    adds dir creation

--- preparation.py
+++ preparation.py
@@ -6,53 +6,61 @@ from file_utils import save_text_to_file, clean_up_files
 
 load_dotenv()  # Load environment variables from .env file
 
+def ensure_directories_exist(*directories):
+    """Ensure that the specified directories exist, creating them if necessary."""
+    for directory in directories:
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+            print(f"Created directory: {directory}")
+
 def process_frames(frames_folder_id, analysis_folder_id, actions_folder_id, system_prompt_path):
     """Process JPG frames in a Google Drive directory and save descriptions."""
+    
+    # Ensure necessary directories exist
+    ensure_directories_exist('frames', 'frame_analysis', 'actions_analysis')
     
     drive_service = authenticate_gdrive()
     jpg_files = list_jpg_files(drive_service, frames_folder_id)
     
+    # Ensure there are at least two frames to process
+    if len(jpg_files) < 2:
+        print("Not enough frames to process.")
+        return
+    
     for i in range(len(jpg_files) - 1):
-        if i == len(jpg_files) - 1: # handle the last file of the sequence
-            download_file(drive_service, file_id1, file_name1)
-            description1 = generate_frame_description(file_name1, system_prompt_path)
-            text_file_name1 = f"{os.path.splitext(file_name1)[0]}.txt"
-            save_text_to_file(text_file_name1, description1)
-            upload_file(drive_service, analysis_folder_id, text_file_name1)
-        else: 
-            file_id1 = jpg_files[i]['id']
-            file_name1 = jpg_files[i]['name']
-            file_id2 = jpg_files[i + 1]['id']
-            file_name2 = jpg_files[i + 1]['name']
-            
-            # Download the frames
-            download_file(drive_service, file_id1, file_name1)
-            download_file(drive_service, file_id2, file_name2)
-            
-            # Generate frame descriptions
-            description1 = generate_frame_description(file_name1, system_prompt_path)
-            description2 = generate_frame_description(file_name2, system_prompt_path)
-            
-            # Save frame descriptions to text files
-            text_file_name1 = f"{os.path.splitext(file_name1)[0]}.txt"
-            text_file_name2 = f"{os.path.splitext(file_name2)[0]}.txt"
-            save_text_to_file(text_file_name1, description1)
-            save_text_to_file(text_file_name2, description2)
-            
-            # Upload the text files to the "frame_analysis" folder
-            upload_file(drive_service, analysis_folder_id, text_file_name1)
-            upload_file(drive_service, analysis_folder_id, text_file_name2)
-            
-            # Generate action description
-            action_description = generate_action_description(file_name1, file_name2, system_prompt_path)
-            
-            # Save action description to a text file
-            action_text_file_name = f"action_{os.path.splitext(file_name1)[0]}_{os.path.splitext(file_name2)[0]}.txt"
-            save_text_to_file(action_text_file_name, action_description)
-            
-            # Upload the action text file to the "actions_analysis" folder
-            upload_file(drive_service, actions_folder_id, action_text_file_name)
-            
+        file_id1 = jpg_files[i]['id']
+        file_name1 = jpg_files[i]['name']
+        file_id2 = jpg_files[i + 1]['id']
+        file_name2 = jpg_files[i + 1]['name']
+        
+        # Download the frames
+        download_file(drive_service, file_id1, file_name1)
+        download_file(drive_service, file_id2, file_name2)
+        
+        # Generate frame descriptions
+        description1 = generate_frame_description(file_name1, system_prompt_path)
+        description2 = generate_frame_description(file_name2, system_prompt_path)
+        
+        # Save frame descriptions to text files
+        text_file_name1 = f"{os.path.splitext(file_name1)[0]}.txt"
+        text_file_name2 = f"{os.path.splitext(file_name2)[0]}.txt"
+        save_text_to_file(text_file_name1, description1)
+        save_text_to_file(text_file_name2, description2)
+        
+        # Upload the text files to the "frame_analysis" folder
+        upload_file(drive_service, analysis_folder_id, text_file_name1)
+        upload_file(drive_service, analysis_folder_id, text_file_name2)
+        
+        # Generate action description
+        action_description = generate_action_description(file_name1, file_name2, system_prompt_path)
+        
+        # Save action description to a text file
+        action_text_file_name = f"action_{os.path.splitext(file_name1)[0]}_{os.path.splitext(file_name2)[0]}.txt"
+        save_text_to_file(action_text_file_name, action_description)
+        
+        # Upload the action text file to the "actions_analysis" folder
+        upload_file(drive_service, actions_folder_id, action_text_file_name)
+        
         # Clean up local files
         clean_up_files(file_name1, file_name2, text_file_name1, text_file_name2, action_text_file_name)
 
