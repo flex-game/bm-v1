ecd857a5fe27c979b834317a532e415a692eeaa0
Jonathan Holtby <holtby.jonathan@gmail.com>
2025-01-21 11:25:56 +0000

    adds some predict imports

--- src/predict.py
+++ src/predict.py
@@ -1,2 +1,12 @@
 import sagemaker
 import tensorflow as tf
+import boto3
+
+s3 = boto3.client('s3')
+
+def download_model(bucket, key, local_path):
+    s3.download_file(bucket, key, local_path)
+
+def upload_model(bucket, key, local_path):
+    s3.upload_file(local_path, bucket, key)
+
